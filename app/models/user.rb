c2e9b148fd0a9500fc6ac474c2a092699c608aa1
Profsain <profsaincrown@gmail.com>
2022-09-28 21:21:01 +0100

    Added user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  # validation
+  validates :name, presence: true
+  validates :posts_counter, numiricality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   # fetch recent top 3 posts
   def fetch_recent_posts
     posts.order(created_at: :desc).limit(3)
