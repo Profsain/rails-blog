7cb25c53cc7f6f48eec077c247c451f603351f9f
Profsain <profsaincrown@gmail.com>
2022-10-03 10:52:17 +0100

    Fixed rubocop error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
   # Get all users
-  def index
-  end
+  def index; end
 
   # Get single user by id
-  def show
-  end
+  def show; end
 end
