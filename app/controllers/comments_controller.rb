5d57c21746da43b9fefc48d71fe44a05846fba72
Profsain <profsaincrown@gmail.com>
2022-10-06 13:04:35 +0100

    Added form controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,14 @@
+class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.author = current_user
+
+    @comment.save if @comment.valid?
+    redirect_to post_path(@comment.post)
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:text, :post_id)
+    end
+end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,4 +10,30 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
   end
+
+  def new; end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+
+    if post.valid?
+      @post.save
+      redirect_to user_posts_path(current_user)
+    else
+      redirect_to new_user_post_path(current_user)
+    end
+  end
+
+  private
+  
+  def fetch_user
+    return nil unless params[:user_id]
+    @user = User.find(params[:user_id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+
 end
